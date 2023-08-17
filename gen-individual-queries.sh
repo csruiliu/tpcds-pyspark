a51cd182bd61783aebb9f84448ad1d1c01044e42
csruiliu <rui.liu@myyahoo.com>
2023-08-16 20:33:43 -0400

    add tpcds toolkit and some modification

---
+++ gen-individual-queries.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TEMPLATEDIR="/home/ruiliu/Develop/tpcds-pyspark/query_templates"
+TOOLSDIR="/home/ruiliu/Develop/tpcds-pyspark/tools"
+OUTDIR="/home/ruiliu/Develop/tpcds-pyspark/queries"
+SCALE=1
+MV_OPTION=" -f "
+
+for i in {1..99}
+do
+    echo "Generating Query $i"
+    cmd="echo query${i}.tpl > $TEMPLATEDIR/templates_${i}.lst"
+    eval "$cmd"
+
+    cmd="$TOOLSDIR/dsqgen -DIRECTORY $TEMPLATEDIR -INPUT $TEMPLATEDIR/templates_${i}.lst -DISTRIBUTIONS $TOOLSDIR/tpcds.idx -VERBOSE Y -QUALIFY Y -SCALE $SCALE -DIALECT netezza -OUTPUT_DIR $OUTDIR"
+    eval "$cmd"
+
+    cmd="mv $MV_OPTION $OUTDIR/query_0.sql $OUTDIR/query${i}.sql"
+    eval "$cmd"
+done
